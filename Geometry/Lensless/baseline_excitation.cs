a37c436ebe87456cc3cabae64be1d5c578cd2e33
tachyonic <kfc.feather@gmail.com>
2015-02-20 17:47:19 -0500

    Lenless - Model: Excitation
    Trying to figure out excitation model

---
+++ Geometry/Lensless/baseline_excitation.cs
@@ -0,0 +1,57 @@
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), BASE_RAYS, THETA_CALC, THETA_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 103            , 11.2224580694, 11.6498306186
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), BASE_RAYS, THETA_CALC, THETA_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 103            , 11.2224580694, 11.6498306186
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), BASE_RAYS, THETA_CALC, THETA_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 104            , 11.2224580694, 11.7064431926
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), BASE_RAYS, THETA_CALC, THETA_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 105            , 11.2224580694, 11.7627870854
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
