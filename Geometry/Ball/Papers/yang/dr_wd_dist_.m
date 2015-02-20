fa0ac89a91bdc2cbef7e8e80c23316a94d25c403
tachyonic <kfc.feather@gmail.com>
2015-02-19 23:02:33 -0500

    Ball - Yang - Analysis: cubic zirconia distribution
    appending sapphire ball version of same simulation

--- Geometry/Ball/Papers/yang/dr_wd_dist_.m
+++ Geometry/Ball/Papers/yang/dr_wd_dist_.m
@@ -5,17 +5,32 @@ clc
 load 'dr_wd_dist_ch25';
 [wdq,drq] = meshgrid(0:50:150,12.5:12.5:112.5);
     
-    dr = dr_wd_dist(:,13);
-    wd = dr_wd_dist(:,10);
-    dd =dr_wd_dist(:,16);
+    dr_s = dr_wd_dist_sapphire(:,13);
+    wd_s = dr_wd_dist_sapphire(:,10);
+    dd_s =dr_wd_dist_sapphire(:,16);
     
-    figure(1);
-    scatter3(wd,dr,dd,'.');
+    dr_c = dr_wd_disr_cubic(:,13);
+    wd_c = dr_wd_disr_cubic(:,10);
+    dd_c =dr_wd_disr_cubic(:,16);
+    
+    subplot(2,1,1);
+    scatter3(wd_s,dr_s,dd_s,'.');
     hold on;
-    ddq = griddata(wd,dr,dd,wdq,drq);
-    mesh(wdq,drq,ddq);
+    ddq_s = griddata(wd_s,dr_s,dd_s,wdq,drq);
+    mesh(wdq,drq,ddq_s);
     hold on;
-    title('Distribution of rays along detector radius vs working distance \nfor channel and lens diameter = 25um');
+    title({'sapphire ball', 'Distribution of rays along detector radius vs working distance', 'for channel and lens diameter = 25um'});
+    ylabel('Working Distance');
+    xlabel('Detector Radius');
+    zlabel('Additional Rays/Additonal Area');
+    
+    subplot(2,1,2);
+    scatter3(wd_c,dr_c,dd_s,'x');
+    hold on;
+    ddq_c = griddata(wd_c,dr_c,dd_c,wdq,drq);
+    mesh(wdq,drq,ddq_c);
+    hold on;
+    title({'cubic zirconia', 'Distribution of rays along detector radius vs working distance', 'for channel and lens diameter = 25um'});
     ylabel('Working Distance');
     xlabel('Detector Radius');
     zlabel('Additional Rays/Additonal Area');
