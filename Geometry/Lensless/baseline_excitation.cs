85403ef6f59776a3e655e02f97b4df4c468440fa
tachyonic <kfc.feather@gmail.com>
2015-02-23 19:15:24 -0500

    Model: Excitation
    tested excitation model with gaussian beam and angled+tapered fiber
    input.

--- Geometry/Lensless/baseline_excitation.cs
+++ Geometry/Lensless/baseline_excitation.cs
@@ -55,3 +55,19 @@
  .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
   
  EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+  
+ EMISSION (um), FLUX_TOTAL, COAT_REFL, CHAN_WIDTH, AIR_GAP (um), WORKING_DISTANCE (um), DET_RAD (um), EMISSION_RAYS, THETA_CALC, THETA_RAYS, EXCITATION_RAYS
+ .59       , 10000        , -1          , 25           , 2         , 250       , 50        , 0              , 11.2224580694, 0            , 0                
