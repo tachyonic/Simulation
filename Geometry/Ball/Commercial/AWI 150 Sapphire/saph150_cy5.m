4f35971d3f06016f14bf0ff946e5a618523ea589
tachyonic <kfc.feather@gmail.com>
2015-02-28 15:31:48 -0500

    Ball - AWI - Analysis
    baseline for previous case with updated calculations
    
    Observations:
    - infinite SNR for all baseline cases means that the cause of increased
    SNR is based on scattering from the ball lens.
    -adding lens still increases collection angle.

--- Geometry/Ball/Commercial/AWI 150 Sapphire/saph150_cy5.m
+++ Geometry/Ball/Commercial/AWI 150 Sapphire/saph150_cy5.m
@@ -12,6 +12,10 @@ dr_all = all_data(:,12);
 wd_all = all_data(:,9);
 theta = all_data(:,19);
 
+dr_base = base_data(:,12);
+wd_base = base_data(:,9);
+theta_base = base_data(:,19);
+
 subplot(2,1,1);
 [wdq,drq] = meshgrid(min(wd_snr):0.5:max(wd_snr),min(dr_snr):2.5:max(dr_snr));
 scatter3(wd_snr,dr_snr,snr,'.');
@@ -32,6 +36,14 @@ hold on;
 thetaq = griddata(dr_all,wd_all,theta,drq,wdq);
 mesh(drq,wdq,thetaq);
 hold on;
+
+[drbq,wdbq] = meshgrid(min(dr_base):2.5:max(dr_base),min(wd_base):0.5:max(wd_base));
+scatter3(dr_base,wd_base,theta_base,'x');
+hold on;
+thetabq = griddata(dr_base,wd_base,theta_base,drbq,wdbq);
+mesh(drbq,wdbq,thetabq);
+hold on;
+
 title({'150um Sapphire Ball', '\theta against Working Distance and Detector Radius'});
 xlabel('DETECTOR RADIUS');
 ylabel('WORKING DISTANCE');
